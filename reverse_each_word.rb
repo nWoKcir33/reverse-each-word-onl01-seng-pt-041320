fb1b64f56c2140b522f827cdd1b7cc17ed0a84be
Rick Lim <rick.jason.a.lim@gmail.com>
2020-05-05 14:45:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,3 +7,11 @@ def reverse_each_word(string)
   return_array.join(" ")
 end
 
+def reverse_each_word(string)
+  original_array = string.split(" ")
+  return_array = []
+  original_array.collect do|string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
+end
